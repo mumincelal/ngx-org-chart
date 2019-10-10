31bbb2a451cc64888e6961492f05798f536961f6
Mumin Celal Pinar <mumincelalpinar@gmail.com>
2019-10-10 22:00:10 +0300

    Cold start. #1

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ngx-org-chart!');
+    expect(page.getTitleText()).toEqual('Welcome to ngx-org-ui!');
   });
 
   afterEach(async () => {
